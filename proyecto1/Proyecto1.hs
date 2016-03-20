bdafa4c8867459a6cfd1f22e042e4c17d2fce0e1
manu-msr <manu@ciencias.unam.mx>
2016-03-19 18:58:09 -0600

    Proyecto1: primer commit

---
+++ proyecto1/Proyecto1.hs
@@ -0,0 +1,8 @@
+--------------------------------------------------------------------------------
+-- Universidad Nacional Autónoma de México                                    --
+-- Facultad de Ciencias                                                       --
+-- Programación Declarativa, 2016-2                                           --
+-- Proyecto 1: Criptoaritmos                                                  --
+-- Manuel Soto Romero                                                         --
+--------------------------------------------------------------------------------
+module Proyecto1 where
