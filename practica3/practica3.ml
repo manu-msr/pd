d7c07be1ccd8c77da06aa33bc1cb3691864bb30f
manu-msr <manu@ciencias.unam.mx>
2016-04-12 16:35:14 -0500

    Ejercicio 1 terminado.

--- practica3/practica3.ml
+++ practica3/practica3.ml
@@ -1,6 +1,18 @@
 (* Práctica 3 *)
 
+
+(* Ejercicio 1 *)
 let rec combinaciones parnp =
 	match parnp with
 	|(n,0.0) -> 1.0
 	|(n,p) -> (n/.p)*.(combinaciones (n -. 1.0,p -. 1.0));;
+
+let rec combinaciones2 parnp =
+	match parnp with
+	| (n, p) -> (combs n p 1.0);;
+
+let rec combs n p acc =
+	if p = 0.0 then
+		acc
+	else
+		(combs (n -. 1.0) (p -. 1.0) (acc *. (n /. p)));;
