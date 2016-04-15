79dad1d9d148a5e356a5e295cf5026bae7d9df75
manu-msr <manu@ciencias.unam.mx>
2016-04-14 23:11:35 -0500

    Ejercicio 7 terminado.

--- practica3/practica3.ml
+++ practica3/practica3.ml
@@ -12,7 +12,7 @@ open String;;
 let rec combinaciones parnp =
    match parnp with
    |(n,0.0) -> 1.0
-   |(n,p) -> (n/.p)*.(combinaciones (n -. 1.0,p -. 1.0));;
+   |(n,p) -> (n/.p)*.(combinaciones (n -. 1.0,p -. 1.0))
 
 (* Función auxiliar que obtiene el número de combinaciones con Recursión de 
    Cola, usa un acumulador. *)
@@ -20,7 +20,7 @@ let rec combs n p acc =
    if p = 0.0 then
       acc
    else
-      (combs (n -. 1.0) (p -. 1.0) (acc *. (n /. p)));;
+      (combs (n -. 1.0) (p -. 1.0) (acc *. (n /. p)))
 
 (* Versión 2 de la función que encuentra el número de combinaciones. *)
 let rec combinaciones2 parnp =
@@ -36,7 +36,7 @@ let rec it_list f e l =
 let rec maximal p l =
    match l with
    | [] -> failwith "Lista vacía"
-   | (x::xs) -> (it_list p (p x (maximal p xs)) xs);;
+   | (x::xs) -> (it_list p (p x (maximal p xs)) xs)
 
 (* Ejercicio 4. *)
 type exp =
@@ -45,7 +45,7 @@ type exp =
    | Sum of exp * exp
    | Prod of exp * exp
    | Div of exp * exp
-   | Exp of exp * int;;
+   | Exp of exp * int
 
 (* Función que toma una expresión y la convierte en cadena. Agregamos paréntesis
    para hacer la expresión más entendible.
@@ -57,7 +57,7 @@ let rec str_of_exp exp =
    | Sum (i,d) -> "(" ^ (str_of_exp i) ^ " + " ^ (str_of_exp d) ^ ")" 
    | Prod (i,d) -> "(" ^ (str_of_exp i) ^ " * " ^ (str_of_exp d) ^ ")"
    | Div (i,d) -> "(" ^ (str_of_exp i) ^ " / " ^ (str_of_exp d) ^ ")"
-   | Exp (b,p) -> "(" ^ (str_of_exp b) ^ "^" ^ (string_of_int p) ^ ")";; 
+   | Exp (b,p) -> "(" ^ (str_of_exp b) ^ "^" ^ (string_of_int p) ^ ")"
 
 (* Función que toma una expresión y devuelve su derivada. 
    derive : exp -> exp *)
@@ -68,4 +68,5 @@ let rec derive exp =
    | Sum (i,d) -> (Sum (derive i, derive d))
    | Prod (i,d) -> (Sum ((Prod (derive i, d)), (Prod (i, derive d))))
    | Div (i,d) -> (Div ((Prod ((Sum ((Prod (derive i,d)),(Prod (i,(derive d))))),(Const (-1)))),(Exp (d,2))))
-   | Exp (b,p) -> (Const 0);;
+   | Exp (b,p) -> (Prod ((Exp ((Prod ((Const p),b),p-1)), (derive b))))
+   
