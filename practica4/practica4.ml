04ed7ddd1a6486bea12d90d62ff0215e81933a78
manu-msr <manu@ciencias.unam.mx>
2016-05-09 23:54:02 -0500

    Ejercicio 2 terminado.

--- practica4/practica4.ml
+++ practica4/practica4.ml
@@ -3,3 +3,14 @@
    ·Alfaro Mendoza Yoshua Ian
    ·Patlani Aguilar Luis Ángel 
    ·Soto Romero Manuel **)
+
+(* Función que itera una función. Esta función toma como argumento una función y
+   un entero y regresa un arreglo de tamaño n donde el elemento i del arreglo es
+   f i.
+   tabular : (int -> 'a) -> int -> 'a array *)
+let tabular f n = 
+	let arreglo = Array.make n (f n) in
+		for i = 0 to (n-1) do
+			arreglo.(i) <- f i
+		done;
+	arreglo
